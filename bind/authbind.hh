fdb3661d8e8b3d0b661e3aa2a47072d1e323d2b6
Justin Lewis Salmon <jsalmon@cern.ch>
2013-01-01 15:09:34 +0000

    Add authbind header file

---
+++ bind/authbind.hh
@@ -0,0 +1,59 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2011-2012 by European Organization for Nuclear Research (CERN)
+// Author: Justin Salmon <jsalmon@cern.ch>
+//------------------------------------------------------------------------------
+// XRootD is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// XRootD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with XRootD.  If not, see <http://www.gnu.org/licenses/>.
+//------------------------------------------------------------------------------
+
+#include <Python.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <dlfcn.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "xrootd/XrdSec/XrdSecInterface.hh"
+#include "xrootd/XrdOuc/XrdOucEnv.hh"
+#include "xrootd/XrdOuc/XrdOucErrInfo.hh"
+#include "xrootd/XrdSys/XrdSysLogger.hh"
+
+
+//------------------------------------------------------------------------------
+// Authentication function handles
+//------------------------------------------------------------------------------
+typedef XrdSecProtocol *(*XrdSecGetProt_t)(const char *, const sockaddr &,
+        const XrdSecParameters &, XrdOucErrInfo *);
+
+typedef XrdSecService *(*XrdSecGetServ_t)(XrdSysLogger *, const char *);
+
+
+XrdSecProtocol *authProtocol;
+XrdOucEnv *authEnv;
+XrdSysLogger logger;
+
+const char *host = "localhost";
+const char *config;
+const char *authLibName;
+void *libHandle;
+
+int sock;
+struct sockaddr_in *sockadd;
+
+std::string protocolName;
+std::stringstream err;
+
