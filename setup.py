cd26a504bd13da84948c1e776f7d0677acb1f475
Lukasz Janyst <ljanyst@cern.ch>
2013-01-10 16:37:02 +0100

    Make sure all the files are properly handled by distutils

--- setup.py
+++ setup.py
@@ -1,8 +1,34 @@
 from distutils.core import setup, Extension
+from os import getenv
 
-setup(name="authbind", version="1.0",
-      ext_modules=[Extension(
-                            "authbind", ["bind/authbind.cc"],
-                            extra_objects=["-lXrdUtils"],
-                            include_dirs=["/usr/include/xrootd"]
-                            )])
+xrdlibdir = getenv( 'XRD_LIBDIR' ) or '/usr/lib'
+xrdincdir = getenv( 'XRD_INCDIR' ) or '/usr/include/xrootd'
+
+print 'XRootD library dir:', xrdlibdir
+print 'XRootD include dir:', xrdincdir
+
+setup( name             = 'XrdImposter',
+       version          = '0.1',
+       author           = 'Justin Salmon, Lukasz Janyst',
+       author_email     = 'jsalmon@cern.ch, ljanyst@cern.ch',
+       url              = 'http://xrootd.org',
+       license          = 'LGPL',
+       scripts          = ['imposter.py'],
+       packages         = ['XrdImposter'],
+       package_dir      = {'XrdImposter': 'lib'},
+       data_files       = [('share/XrdImposter/examples',
+                            ['examples/XRootDLogInClient.py',
+                             'examples/XRootDLogInServer.py'])],
+       description      = "Implementation of the XRootD protocol",
+       long_description = "Implementation of the XRootD protocol",
+       ext_modules      = [
+           Extension(
+               'XrdImposter.XrdAuthBind',
+               ['auth/XrdAuthBind.cc'],
+               libraries    = ['XrdUtils', 'dl'],
+               extra_compile_args = ['-g'],
+               include_dirs = [xrdincdir],
+               library_dirs = [xrdlibdir]
+               )
+           ]
+       )
